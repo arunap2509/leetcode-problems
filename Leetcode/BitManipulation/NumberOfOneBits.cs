0b8701e13e9bf502bbe15afba475690c676fa79b
Arun AP <arunap@192.168.0.103>
2022-07-24 22:29:13 +0530

    bitmanipulation problem

---
+++ Leetcode/BitManipulation/NumberOfOneBits.cs
@@ -0,0 +1,18 @@
+﻿namespace Leetcode.BitManipulation;
+
+public class NumberOfOneBits
+{
+    public static int HammingWeight(uint n)
+    {
+        int ans = 0;
+
+        while (n != 0)
+        {
+            n &= (n - 1);
+            ans++;
+        }
+
+        return ans;
+    }
+}
+
---
+++ Leetcode/Miscellaneous/SubtractProductAndSumOfDigit.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.Miscellaneous;
+
+public class SubtractProductAndSumOfDigit
+{
+    public static int SubtractProductAndSum(int n)
+    {
+        if (n == 0) return n;
+
+        var product = 1;
+        var sum = 0;
+
+        while (n != 0)
+        {
+            var digit = n % 10;
+            n /= 10;
+
+            product *= digit;
+            sum += digit;
+        }
+
+        return product - sum;
+    }
+}
+
