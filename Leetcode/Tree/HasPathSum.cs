f584e9bdf42bbea7acc26d51ceea062f075b7957
Arun AP <arunap@192.168.0.103>
2022-07-29 23:27:54 +0530

    path sum and invert binary tree

---
+++ Leetcode/Tree/HasPathSum.cs
@@ -0,0 +1,39 @@
+﻿#pragma warning disable CS8604
+
+namespace Leetcode.Tree;
+
+public class HasPathSum
+{
+    public bool Solution(TreeNode root, int targetSum)
+    {
+        return HasPathSumHelper(root, targetSum, 0);
+    }
+
+    private bool HasPathSumHelper(TreeNode root, int targetSum, int currentSum)
+    {
+        if (root == null) return false;
+
+        currentSum += root.val;
+
+        if (root.left == null && root.right == null && currentSum == targetSum)
+        {
+            return true;
+        }
+
+        return HasPathSumHelper(root.left, targetSum, currentSum)
+            || HasPathSumHelper(root.right, targetSum, currentSum);
+    }
+
+    // dont always have to add or bring in another variable
+    // can also work with subtraction
+    public bool AnotherSolution(TreeNode root, int targetSum)
+    {
+        if (root == null) return false;
+
+        if (root.left == null && root.right == null && targetSum - root.val == 0) return true;
+
+        return AnotherSolution(root.left, targetSum - root.val)
+            || AnotherSolution(root.right, targetSum - root.val);
+    }
+}
+
---
+++ Leetcode/Tree/InvertTree.cs
@@ -0,0 +1,57 @@
+﻿#pragma warning disable CS8603
+
+namespace Leetcode.Tree;
+
+public class InvertTree
+{
+    public static TreeNode Solution(TreeNode root)
+    {
+        if (root == null) return root;
+
+        Stack<TreeNode> stack = new();
+
+        var result = root;
+
+        stack.Push(root);
+
+        while (stack.Any())
+        {
+            root = stack.Pop();
+
+            (root.right, root.left) = (root.left, root.right);
+            if (root.right != null) stack.Push(root.right);
+            if (root.left != null) stack.Push(root.left);
+        }
+
+        return result;
+    }
+
+    public TreeNode RecursiveSolution(TreeNode root)
+    {
+        if (root == null)
+        {
+            return root;
+        }
+
+        var left = root.left;
+        root.left = RecursiveSolution(root.right);
+        root.right = RecursiveSolution(left);
+
+        return root;
+
+    }
+
+    public TreeNode AnotherRecursiveSolution(TreeNode root)
+    {
+        if (root == null) return root;
+
+        TreeNode left = AnotherRecursiveSolution(root.left);
+        TreeNode right = AnotherRecursiveSolution(root.right);
+
+        root.right = left;
+        root.left = right;
+
+        return root;
+    }
+}
+
