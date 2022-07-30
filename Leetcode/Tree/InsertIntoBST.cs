f8fd6f804d1af1ca11f6b8f6574559f947f9d0c4
Arun AP <arunap@192.168.0.103>
2022-07-30 09:23:42 +0530

    few more bst problems

---
+++ Leetcode/Tree/InsertIntoBST.cs
@@ -0,0 +1,59 @@
+﻿namespace Leetcode.Tree;
+
+public class InsertIntoBST
+{
+    public TreeNode RecursiveSolution(TreeNode root, int val)
+    {
+
+        if (root == null) return new(val);
+
+        if (root.val > val)
+        {
+            root.left = RecursiveSolution(root.left, val);
+        }
+        else
+        {
+            root.right = RecursiveSolution(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static TreeNode IterativeSolution(TreeNode root, int val)
+    {
+        if (root == null) return new(val);
+
+        var result = root;
+
+        while(true)
+        {
+            if (root.val > val)
+            {
+                if (root.left == null)
+                {
+                    root.left = new(val);
+                    break;
+                }
+                else
+                {
+                    root = root.left;
+                }
+            }
+            else
+            {
+                if (root.right == null)
+                {
+                    root.right = new(val);
+                    break;
+                }
+                else
+                {
+                    root = root.right;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
---
+++ Leetcode/Tree/SearchBST.cs
@@ -0,0 +1,46 @@
+﻿#pragma warning disable CS8603
+
+namespace Leetcode.Tree;
+
+public class SearchBST
+{
+    public static TreeNode IterativeSolution(TreeNode root, int val)
+    {
+
+        if (root == null) return root;
+
+        Stack<TreeNode> stack = new();
+
+        stack.Push(root);
+
+        while (stack.Any())
+        {
+            var current = stack.Pop();
+
+            if (current.val == val) return current;
+
+            if (current.left != null) stack.Push(current.left);
+
+            if (current.right != null) stack.Push(current.right);
+        }
+
+        return null;
+    }
+
+    public TreeNode RecursiveSolution(TreeNode root, int val)
+    {
+
+        if (root == null) return root;
+
+        if (root.val == val) return root;
+
+        var leftSearch = RecursiveSolution(root.left, val);
+
+        if (leftSearch != null) return leftSearch;
+
+        var rightSearch = RecursiveSolution(root.right, val);
+
+        return rightSearch;
+    }
+}
+
